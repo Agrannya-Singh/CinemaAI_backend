4cdd6c111931b846e76f7be81290841271e082d3
agrannya <singh.agrannya@gmail.com>
2025-09-15 09:49:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,5 +18,6 @@ OMDB_API_URL = "http://www.omdbapi.com/"
 API_TIMEOUT_SECONDS = 10
 
 # --- Model Weights ---
-CONTENT_WEIGHT = 0.5
-COLLAB_WEIGHT = 0.5
+CONTENT_WEIGHT = 0.8
+COLLAB_WEIGHT = 0.2 
+#collab model is how other users liked movies #before the current user. as userbase #increase we increase collab weight 
